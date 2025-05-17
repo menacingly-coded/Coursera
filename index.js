e241dd5f604a0b75ad64743fb39f3f4b3c75719b
Shreya Sharma <shreya0987.edu@gmail.com>
2025-05-17 14:21:24 +0530

    Step 7 - only start if databse is up

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const {userRouter} = require("./routes/user")
 const {courseRouter} = require("./routes/course")
 const {adminRouterRouter} = require("./routes/admin")
+const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 3000
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
-app.listen(3000);
+
+async function main(){
+  await mongoose.connect("mongodb+srv://shreya0987edu:<db_password>@cluster0.2raftwq.mongodb.net/")
+  app.listen(3000);
+  console.log("listening to port 3000");
+}
