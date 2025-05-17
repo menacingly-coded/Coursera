cd78e6639a6aa1e741d4c6743dcbb3386167b3c7
Shreya Sharma <shreya0987.edu@gmail.com>
2025-05-17 14:30:01 +0530

    added minimal changes

--- routes/admin.js
+++ routes/admin.js
@@ -14,19 +14,19 @@ res.json({
 })
 })
 
-adminRouter.post("/course", function(req, res){
+adminRouter.post("/", function(req, res){
 res.json({
     message: "signup endpoint" 
 })
 })
 
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/", function(req, res){
 res.json({
     message: "signup endpoint" 
 })
 })
 
-adminRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/bulk", function(req, res){
 res.json({
     message: "signup endpoint" 
 })
