d2c37fec634c7672dd117225c735f5a60416030d
Shreya Sharma <shreya0987.edu@gmail.com>
2025-05-17 12:34:50 +0530

    Step 4 - added course router

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const {courseRouter} = require("./routes/course")
 const app = express()
 const port = 3000
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000);
--- routes/course.js
+++ routes/course.js
@@ -3,13 +3,15 @@ const {Router} = require("express");
 const courseRouter = Router();
 
 courseRouter.post("/purchase", function(req, res){
-res.json({
-    message: "signup endpoint" 
-})
+    res.json({
+        message: "signup endpoint" 
+    })
 })
 
 courseRouter.get('/preview', (req, res) => {
-res.send('Hello World!')
+    res.json({
+        message: "coursepreview endpoint" 
+    })
 })
 
 
