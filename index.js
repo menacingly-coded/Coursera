8a37650dccd5727b2515663f649ff480a004f5aa
Shreya Sharma <shreya0987.edu@gmail.com>
2025-05-17 12:13:04 +0530

    Step 3 - added Routers

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express')
+const {userRouter} = require("./routes/user")
+const {courseRouter} = require("./routes/course")
 const app = express()
 const port = 3000
 
-app.post("/user/signup", function(req, res){
-  res.json({
-    message: "signup endpoint" 
-  })
-})
-
-app.post("/user/signin", function(req, res){
-  res.json({
-    message: "signup endpoint" 
-  })
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
